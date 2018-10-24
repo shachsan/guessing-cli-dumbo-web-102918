41f9e133c0cb3c3119b3c727e776e442060f5f31
SANJAY K SAI <shachsan@hotmail.com>
2018-10-24 14:37:02 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,45 +7,50 @@ def computer_num
   return rand(1..6)
 end
 #
-# def exit?
-#   player_selection = gets.chomp
-#   if player_selection == "exit"
-#     puts "Goodbye!"
-#     return true
-#   else
-#     return false
-#   end
-# end
+def exit?
+  player_selection = gets.chomp
+  if player_selection == "exit"
+    puts "Goodbye!"
+    return true
+  else
+    return false
+  end
+end
 #
 def run_guessing_game
   puts "Welcome to Guess Game"
   puts "(Pleae note: You could exit this game after finishing a started game by enter 'exit'.)"
-  # binding.pry
+  
   loop do
     my_num = computer_num
     puts "Can you guess my number between 1 and 6?"
-    you_guessed = gets.chomp.to_i
-    if you_guessed > 6 || you_guessed < 1
+    you_guessed = gets.chomp
+    # 
+    # if you_guessed == "exit"
+    #   break
+    # end 
+    
+    if you_guessed.to_i > 6 || you_guessed.to_i < 1
       puts "Invalid guess, please enter the number between 1 and 6"
       next
     end
 
-    if you_guessed == my_num
+    if you_guessed.to_i == my_num
       puts "You guessed the correct number!"
-      player_selection = gets.strip
-      if player_selection == "exit"
-        puts "Goodbye!"
-        break
-      end
-      # break if exit?
+      # player_selection = gets.strip
+      # if player_selection == "exit"
+      #   puts "Goodbye!"
+      #   break
+      # end
+      break if exit?
     else
       puts "The computer guessed #{my_num}."
-      player_selection = gets.strip
-      if player_selection == "exit"
-        puts "Goodbye!"
-        break
-      end
-      # break if exit?
+      # player_selection = gets.strip
+      # if player_selection == "exit"
+      #   puts "Goodbye!"
+      #   break
+      # end
+      break if exit?
     end
 
   end
