9bc7b7833897efcd56bbb5e6206b55a335339dc4
SANJAY K SAI <shachsan@hotmail.com>
2018-10-24 15:05:38 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,36 +1,28 @@
-# Code your solution here!
-def computer_num
-  rand(1..6)
-end
-
-def exit?
-  player_selection = gets.chomp
-  if player_selection == "exit"
-    puts "Goodbye!"
-    return true
-  else
-    return false
-  end
-end
+# Language: Ruby, Level: Level 3
 
 def run_guessing_game
-  puts "Welcome to Guess Game"
-  puts "(Pleae note: You could exit this game anytime after the first game by enter 'exit'.)"
-  loop do
-    my_num = computer_num
-    puts "Can you guess my number between 1 and 6?"
-    you_guessed = gets.chomp.to_i
-    if you_guessed > 6 || you_guessed < 1
+
+  you_guessed = ""
+
+  while true
+    my_num = rand(1..6)
+    puts "Guess a number between 1 and 6."
+    you_guessed = gets.chomp
+    #
+    if you_guessed == "exit"
+      puts "Goodbye!"
+      break
+    end
+
+    if you_guessed.to_i > 6 || you_guessed.to_i < 1
       puts "Invalid guess, please enter the number between 1 and 6"
       next
     end
 
-    if you_guessed == my_num
+    if you_guessed.to_i == my_num
       puts "You guessed the correct number!"
-      break if exit?
     else
       puts "The computer guessed #{my_num}."
-      break if exit?
     end
 
   end
