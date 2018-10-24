4d0afbf93a8cff24bd50c09f046b602f904edb14
SANJAY K SAI <shachsan@hotmail.com>
2018-10-24 14:56:34 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,28 +1,11 @@
-# Language: Ruby, Level: Level 3
 
-require 'pry'
-# Language: Ruby, Level: Level 3
-# # Code your solution here!
-def computer_num
-  return rand(1..6)
-end
-#
-def exit?
-  player_selection = gets.chomp
-  if player_selection == "exit"
-    puts "Goodbye!"
-    return true
-  else
-    return false
-  end
-end
 #
 def run_guessing_game
 
   you_guessed=""
 
   while you_guessed!="exit"
-    my_num = computer_num
+    my_num = rand(1..6)
     puts "Guess a number between 1 and 6."
     you_guessed = gets.chomp
     #
@@ -38,20 +21,8 @@ def run_guessing_game
 
     if you_guessed.to_i == my_num
       puts "You guessed the correct number!"
-      # player_selection = gets.strip
-      # if player_selection == "exit"
-      #   puts "Goodbye!"
-      #   break
-      # end
-      break if exit?
     else
       puts "The computer guessed #{my_num}."
-      # player_selection = gets.strip
-      # if player_selection == "exit"
-      #   puts "Goodbye!"
-      #   break
-      # end
-      break if exit?
     end
 
   end
